a84648f32893a3a9f928746cf0a1afe7a1049a49
Dawood Khan <152021471+Dwukn@users.noreply.github.com>
2024-10-05 18:55:34 +0530

    Added Nabar to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import { CryptoPriceTracker } from './components/CryptoPriceTracker';
-
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
+        <Navbar />
     <CryptoPriceTracker/>
     </div>
   );
